12a9dd8931aa5eb472b51f7bb39bc16dbed9f353
lirismankarina <lirisman.karina@huawei-partners.com>
2024-05-25 01:02:34 +0300

    attempt to run aal

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Компиляция нативной библиотеки
-g++ -shared -o libMyCppProgram.so MyCppProgram.cpp MyCppWrapper.cpp -I"/usr/lib/jvm/java-8-openjdk-amd64/include" -I"/usr/lib/jvm/java-8-openjdk-amd64/include/linux" -fPIC
+cp ../build/libMyCppProgram.so ./
 
 # Компиляция Java файлов
 javac MyCppWrapper.java
 javac MyApplet.java
 
-# Генерация заголовочного файла JNI
+# Генерация заголовочного файла JNI (если это нужно, но обычно это делается только один раз)
 javah -jni MyCppWrapper
 
 # Вывод "Good!" для проверки успешного выполнения
 echo "Good!"
 
-# Запуск апплета
+# Запуск апплета с указанием пути к нативной библиотеке
 echo "Run Applet"
-appletviewer -J-Djava.security.policy=applet.policy index.html
+appletviewer -J-Djava.library.path=. -J-Djava.security.policy=applet.policy index.html
